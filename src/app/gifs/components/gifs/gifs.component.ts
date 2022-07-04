72d480a37b1d9709b7408528e4d3e4617b884325
Carlos Zabala <czabala847@gmail.com>
2022-07-04 14:18:02 -0500

    Implementando petición a la api de gifs y tipando respuestas

--- src/app/gifs/components/gifs/gifs.component.ts
+++ src/app/gifs/components/gifs/gifs.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-gifs',
   templateUrl: './gifs.component.html',
-  styleUrls: ['./gifs.component.css']
+  styleUrls: ['./gifs.component.css'],
 })
 export class GifsComponent implements OnInit {
+  constructor(private gifsService: GifsService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  get gifs() {
+    return this.gifsService.gifs;
   }
-
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Gif, GifResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
   private _history: string[] = [];
+  public gifs: Gif[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   public add(query: string = '') {
     query = query.trim().toLowerCase();
@@ -14,6 +17,14 @@ export class GifsService {
     if (!this._history.includes(query)) {
       this._history.unshift(query);
       this._history = this._history.splice(0, 10);
+
+      this.http
+        .get<GifResponse>(
+          `https://api.giphy.com/v1/gifs/search?api_key=7qR8P0RDtEj2brc5qQpLFKgbiHlWa6Gf&q=${query}&limit=10`
+        )
+        .subscribe((response) => {
+          this.gifs = response.data;
+        });
     }
   }
 
